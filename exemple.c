c3c192a42bda6a3a45dac99bfedbabdea1da6596
carado <carado@savhon.org>
2013-09-08 15:24:09 +0200

    .

--- exemple.c
+++ exemple.c
@@ -1,11 +1,12 @@
-#include "esstd.tête"
-#include "bibstd.tête"
+#include "esstd.tête" // Entrées-sorties standard
+#include "bibstd.tête" // Bibliothèque standard
 
+// Fonction principale, qui affiche ses arguments puis quitte
 ent principal(ent carg, car** varg) {
-	ent i;
-	pour(i = 0; i < carg; i++)
-		imprimerf("%s ", varg[i]);
-	mettrech("");
-	retourner SORTIE_SUCCÈS;
+	ent i; // Déclaration d’entier
+	pour(i = 0; i < carg; i++) // Boucle «pour»
+		imprimerf("%s ", varg[i]); // Affichage formaté
+	mettrech(""); // Retour à la ligne
+	retourner SORTIE_SUCCÈS; // Fin
 }
 
