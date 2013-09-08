c6a55169dfba7d068a203a0cffcc7e4d85cead92
carado <carado@savhon.org>
2013-09-08 15:23:03 +0200

    .

--- exemple.c
+++ exemple.c
@@ -1,7 +1,7 @@
 #include "esstd.tête"
 #include "bibstd.tête"
 
-ent principal(entier carg, car** varg) {
+ent principal(ent carg, car** varg) {
 	ent i;
 	pour(i = 0; i < carg; i++)
 		imprimerf("%s ", varg[i]);
